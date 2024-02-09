05d44b070e8ed277108f571d7eb16e2cd6293ebd
Karolina <karolina.bukowskaa@gmail.com>
2024-02-09 15:10:31 +0100

    Remove unused code

--- src/test/java/data/UserLoginData.java
+++ src/test/java/data/UserLoginData.java
@@ -11,18 +11,8 @@ public class UserLoginData {
     public String getEmailAddress() {
         return emailAddress;
     }
-
     public String getPassword() {
         return password;
     }
 
-    public UserLoginData(String emailAddress, String password) {
-        this.emailAddress = emailAddress;
-        this.password = password;
-    }
-
-    public UserLoginData() {
-        this.emailAddress = null;
-        this.password = null;
-    }
 }
